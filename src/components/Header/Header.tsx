9a7a3cdf1e9367fdd9a3877c9952d924e7464b5f
Alex GH <110792798+alex070822@users.noreply.github.com>
2023-06-27 20:45:11 -0600

    Add basic project layout

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+function Header() {
+  return (
+    <header className="header">
+      <div className="logo"></div>
+      <div className="header-title">Alexis González</div>
+      <img
+        src={"./src/assets/hamburger_menu.png"}
+        alt="nav menu icon"
+        className="nav-menu"
+      />
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,22 @@
+function Home() {
+  return (
+    <div className="home">
+      <p className="greeting">Hi, my name is</p>
+      <p className="section-title">Alexis González.</p>
+      <p className="quote">
+        I thrive on the challenges that come with turning concepts into tangible
+        solutions.
+      </p>
+      <p className="home-paragraph">
+        I'm a México-based full-stack developer with a passion for crafting
+        digital solutions. I have always been fascinated by the power of
+        technology and its ability to shape the world we live in. Today, I
+        channel that passion into building innovative and user-friendly software
+        applications.
+      </p>
+      <button className="content-button">ABOUT ME</button>
+    </div>
+  );
+}
+
+export default Home;
