0db42eeafaa9be6c89a7f88939c1c4a56a258db4
Alex GH <110792798+alex070822@users.noreply.github.com>
2023-08-16 18:53:02 -0600

    Create new file projectsData.ts

---
+++ src/data/projectsData.ts
@@ -0,0 +1,40 @@
+let projectsData = [
+  {
+    title: "Launch countdown timer",
+    description:
+      "A responsive launch countdown timer that i made using React, Vite and Emotion.",
+    imgSrc: "./src/assets/countdown.png",
+    liveDemo: "https://taupe-bonbon-fc5739.netlify.app/",
+    githubLink:
+      "https://github.com/Alex070822/frontend-mentor/tree/main/launch-countdown-timer",
+  },
+  {
+    title: "Ip address tracker",
+    description:
+      "A responsive IP address tracker made with React, Vite, Emotion and two different APIs.",
+    imgSrc: "./src/assets/ip-tracker.png",
+    liveDemo: "https://steady-panda-cd2ef5.netlify.app/",
+    githubLink:
+      "https://github.com/Alex070822/frontend-mentor/tree/main/ip-address-tracker",
+  },
+  {
+    title: "Planets fact site",
+    description:
+      "A responsive 8-page fact site for all planets in our solar system using React, Vite and Emotion.",
+    imgSrc: "./src/assets/planets.png",
+    liveDemo: "https://warm-licorice-4a2442.netlify.app/",
+    githubLink:
+      "https://github.com/Alex070822/frontend-mentor/tree/main/planets-fact-site",
+  },
+  {
+    title: "Time tracking dashboard",
+    description:
+      " A responsive time tracking dashboard using React and a JSON file to manage data.",
+    imgSrc: "./src/assets/time-tracking-dashboard.png",
+    liveDemo: "https://remarkable-crepe-2bd39c.netlify.app/",
+    githubLink:
+      "https://github.com/Alex070822/frontend-mentor/tree/main/time-tracking-dashboard",
+  },
+];
+
+export default projectsData;
